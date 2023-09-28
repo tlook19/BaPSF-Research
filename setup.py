30b8c9de7107e823d239e580f56d523e73b4a2df
Tom <tlook@physics.ucla.edu>
2023-09-27 19:28:35 -0700

    small fix to setup

--- setup.py
+++ setup.py
@@ -4,18 +4,18 @@ from setuptools import setup
 here = os.path.abspath(os.path.dirname(__file__))
 
 setup(
-    name='bapsfda',
-    version='0.1.0',
-    description='An data analysis package for BaPSF/LAPD',
-    author='Thomas R. Look'
-    author_email='tlook@physics.ucla.edu',
-    packages=['bapsfda'],
+    name="bapsfda",
+    version="0.1.0",
+    description="An data analysis package for BaPSF/LAPD",
+    author="Thomas R. Look",
+    author_email="tlook@physics.ucla.edu",
+    packages=["bapsfda"],
     install_requires=[
-        'numpy',
-        'scipy',
-        'matplotlib',
-        'astropy',
-        'pycine',
-        'scaleogram'
-    ]
+        "numpy",
+        "scipy",
+        "matplotlib",
+        "astropy",
+        "pycine",
+        "scaleogram",
+    ],
 )
