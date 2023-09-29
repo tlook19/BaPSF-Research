2a493e271a267106274e7f571ef857dbfd66d9d2
Tom <tlook@physics.ucla.edu>
2023-09-28 17:19:39 -0700

    still fixing, think I got it tho

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ here = os.path.abspath(os.path.dirname(__file__))
 
 setup(
     name="bapsfda",
-    version="0.1.1d",
+    version="0.1.2",
     description="An data analysis package for BaPSF/LAPD",
     author="Thomas R. Look",
     author_email="tlook@physics.ucla.edu",
