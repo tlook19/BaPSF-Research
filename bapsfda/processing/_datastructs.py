c76d1776933ce8f9a2ee60cd87511abb4f401688
Tom <tlook@physics.ucla.edu>
2023-10-11 19:56:32 -0700

    Simplify Data Structures

--- bapsfda/processing/_datastructs.py
+++ bapsfda/processing/_datastructs.py
@@ -1,18 +1,80 @@
-__all__ = [
-    "SignalArray",
-    "DaqArray",
-    "CurrentDaqArray",
-    "VoltageDaqArray",
-]
+__all__ = ["SignalArray", "DaqArray", "CurrentDaqArray", "VoltageDaqArray"]
 
 import numpy as np
 from astropy import units as u
 from ._datafuncs import butt_low, sav_smooth
 from abc import ABC, abstractmethod
+from typing import Optional
+from dataclasses import dataclass
+
+
+@dataclass
+class RunProperties:
+    shots_total: int
+    trigger_delay: float = 0.0
+
+
+@dataclass
+class DriveProperties:
+    shots_per_pos: int
+    nx: int = 1
+    ny: int = 1
+    nz: int = 1
+
+
+@dataclass
+class BoardProperties:
+    samples_per_shot: int
+    adc_clk: float = 100e6
+    sample_avg: int = 1
+
+
+@dataclass
+class ChannelProperties:
+    gain: float = 1
+    resistance: Optional[float] = None
 
 
 class SignalArray(ABC):
     pass
+
+
+class DataRun:
+    """
+    Class for an LAPD data run with SiS Crate Digitizer DAQ system.
+    The LAPD data run is a collection of signal arrays where the each "Board" has a base ADC clock frequency, number of samples averaged, and number of samples per shot.
+    Each board has 8 channels the each digitize a voltage signal. Each signal may have some gain or resistance associated with it.
+    A typical run will take a number of shots at each probe position, then move to the next probe position,
+    starting from the top left (ymax, xmin) and scanning from left to right, then top to bottom.
+
+    A drive is a collection of channels (which may be on different boards) that are moved together.
+
+    Run Properties that stay for the entire run are:
+        - shots_total: total number of shots in the run
+        - trigger_delay: Delay between LAPD 1kA discharge current trigger and the DAQ trigger in seconds(usually 0)
+
+    Drive Props that are the same for the entire datarun are nx * ny * nz * shots_per_pos should equal shots_total: (assumes grad)
+        - shots_per_pos: number of shots at each probe position
+        - ny: number of probe positions in the y direction
+        - nx: number of probe positions in the x direction
+        - nz: number of probe positions in the z direction
+
+    Board properties are:
+        - adc_clk: ADC clock frequency in Hz (100e6 for LAPD DAQ)
+        - sample_avg: Number of samples averaged by the DAQ (base 2 number for LAPD DAQ)
+        - ns: number of samples per shot
+
+    Channel properties are:
+        - gain: gain of the measurement circuit to be divided out
+        - resistance(optional): resistance in Ohms of the current sense resistor
+
+    From a composition standpoint a datarun is a collection of boards, and each board is a collection of channels.
+
+    In the future, it may be relevent that an experiment is a collection of data runs.
+
+    Returns:
+        _type_: _description_
+    """
 
 
 class DaqArray(SignalArray):
@@ -31,34 +93,39 @@ class DaqArray(SignalArray):
     def __init__(
         self,
         signal: np.ndarray,
-        adc_clk: float,
-        sample_avg: int,
-        ny: int,
-        nx: int,
-        nshots: int,
+        adc_clk: float = 100e6,
+        sample_avg: int = 1,
+        ny: int = 1,
+        nx: int = 1,
+        nshots: int = 1,
+        gain: float = 1,
+        resistance: Optional[float] = None,
     ) -> None:
         """Initialize a DAQ array with a raw signal array and the DAQ run properties.
 
         Args:
             signal (np.ndarray): A raw signal array from the DAQ
-            adc_clk (float): DAQ ADC clock frequency in Hz (1e6 for LAPD DAQ)
+            adc_clk (float): DAQ ADC clock frequency in Hz (100e6 for LAPD DAQ)
             sample_avg (int): Number of samples averaged by the DAQ (base 2 number for LAPD DAQ)
             ny (int): Number of probe positions in the y direction
             nx (int): Number of probe positions in the x direction
             nshots (int): Number of shots per probe position
+            gain (float): Gain of the measurement circuit to be divided out
+            resistance (float): Resistance in ohms of the current sense resistor
         """
-        self._signal_array = signal * u.V
-        self._run_props = {
-            "sample_freq": adc_clk / sample_avg * u.Hz,
-            "ny": ny,
-            "nx": nx,
-            "nshots": nshots,
-            "dt": sample_avg / adc_clk * u.s,
-            "nt": self._signal_array.shape[-1],
-        }
-        self._time_array = (
-            np.arange(self._run_props["nt"]) * self._run_props["dt"]
-        ).to(u.s)
+        self._signal_array = signal / gain
+        self._units = u.V
+        if resistance is not None:
+            self._signal_array = self._signal_array / resistance
+            self._units = u.A
+        self._sample_freq = adc_clk / sample_avg * u.Hz
+        self._ny = ny
+        self._nx = nx
+        self._nshots = nshots
+        self._dt = (self._sample_freq**-1).to(u.s)
+        self._nt = self._signal_array.shape[-1]
+        self._units = u.V
+        self._time_array = (np.arange(self._nt) * self._dt).to(u.s)
 
     @property
     def signal_array(self) -> np.ndarray:
@@ -69,72 +136,33 @@ class DaqArray(SignalArray):
         return self._time_array
 
     @property
-    def run_props(self) -> dict:
-        return self._run_props
+    def sample_freq(self) -> u.Quantity:
+        return self._sample_freq
+
+    @property
+    def dt(self) -> u.Quantity:
+        return self._dt
+
+    @property
+    def nt(self) -> int:
+        return self._nt
+
+    @property
+    def ny(self) -> int:
+        return self._ny
+
+    @property
+    def nx(self) -> int:
+        return self._nx
+
+    @property
+    def nshots(self) -> int:
+        return self._nshots
+
+    @property
+    def units(self) -> u.Unit:
+        return self._units
 
     @property
     def shape(self) -> tuple:
         return self._signal_array.shape
-
-    def reshape(self) -> np.ndarray:
-        return self._signal_array.reshape(
-            self._run_props["ny"],
-            self._run_props["nx"],
-            self._run_props["nshots"],
-            self._run_props["nt"],
-        )
-
-    def low_pass_filter(self, cutoff, order=4) -> np.ndarray:
-        return butt_low(
-            self._signal_array, cutoff, self._run_props["sample_freq"], order
-        )
-
-
-class CurrentDaqArray(DaqArray):
-    def __init__(
-        self,
-        signal: np.ndarray,
-        adc_clk: float,
-        sample_avg: int,
-        ny: int,
-        nx: int,
-        nshots: int,
-        gain,
-        resistance,
-    ) -> None:
-        """Take a raw DAQ signal array and convert it to a current array in Amps.
-
-        Args:
-            gain (float): gain of the measurement circuit to be divided out
-            resistance (float): resistance in ohms of the current sense resistor
-
-        Raises:
-            Exception: _description_
-        """
-        if resistance.unit != u.ohm:
-            raise Exception("Sense resistance must be in ohms.")
-        super().__init__(signal, adc_clk, sample_avg, ny, nx, nshots)
-        self._run_props["gain"] = gain
-        self._run_props["resistance"] = resistance
-        self._signal_array = (self._signal_array / (gain * resistance)).to(u.A)
-
-
-class VoltageDaqArray(DaqArray):
-    def __init__(
-        self,
-        signal: np.ndarray,
-        adc_clk: float,
-        sample_avg: int,
-        ny: int,
-        nx: int,
-        nshots: int,
-        gain: float,
-    ) -> None:
-        """Take a raw DAQ signal array and convert it to a voltage array in Volts.
-
-        Args:
-            gain (float): gain of the measurement circuit to be divided out
-        """
-        super().__init__(signal, adc_clk, sample_avg, ny, nx, nshots)
-        self._run_props["gain"] = gain
-        self._signal_array = self._signal_array / gain
